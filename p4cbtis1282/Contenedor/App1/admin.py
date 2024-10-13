3fac2d5b889d35c9f53772d2bb230b8f17606c41
Ever-Daniel-Pereyra-Castillo <a.22308051281282@cbtis128.edu.mx>
2024-10-13 17:26:32 -0600

    Delete p4cbtis1282 directory

--- p4cbtis1282/Contenedor/App1/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import VENTAS
-# Register your models here.
-
-class VentasAdmin(admin.ModelAdmin):
-    fields = ["id","cantidad","precio","vendedor","impuestos","comprador","fecha"]
-    list_display =["cantidad","fecha"]
-    
-admin.site.register(VENTAS, VentasAdmin)
